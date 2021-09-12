8fed38fe7009718b02ec0acdbc22ceb4dafce188
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-09-12 21:09:18 +0200

    Create int_set solution

---
+++ Ruby/LRU cache/lib/p01_int_set.rb
@@ -0,0 +1,112 @@
+class MaxIntSet
+  
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.length - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num].push(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num) 
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+# time complexity for insert, remove, include is O(1) amortized. Except for some pathological input sets
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    # calls resize! when enough elements are inserted
+    resize! if count == num_buckets
+    return if self.include?(num)
+    self[num] << num
+    @count += 1
+  end
+
+  def remove(num)
+    return unless self.include?(num)
+    self[num].delete(num)
+    @count -= 1
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    temp = @store
+    k = num_buckets * 2
+    @store = Array.new(k) { Array.new }
+    temp.each do |bucket|
+      bucket.each do |ele|
+        self[ele] << ele  
+      end
+    end
+  end
+
+end
