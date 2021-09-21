b8abad6556b04d3bdda7a8239d1492512fd4eacc
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-09-21 17:01:42 +0200

    Create course model

--- SQL/associations_exercise/app/models/course.rb
+++ SQL/associations_exercise/app/models/course.rb
@@ -1,3 +1,20 @@
 class Course < ApplicationRecord
-    
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :user 
+
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 end
