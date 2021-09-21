2fbb48154fbea5d0b70be6563d6bd53a65f237f7
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-09-21 17:00:30 +0200

    Create enrollment model

--- SQL/associations_exercise/app/models/enrollment.rb
+++ SQL/associations_exercise/app/models/enrollment.rb
@@ -1,3 +1,11 @@
 class Enrollment < ApplicationRecord
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User 
     
+    belongs_to :course,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course 
 end
