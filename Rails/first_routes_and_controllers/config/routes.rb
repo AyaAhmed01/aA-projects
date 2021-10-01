beeaceba040dea5beb010495ecf65ef03b960816
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-10-01 23:53:07 +0200

    Create routes

---
+++ Rails/first_routes_and_controllers/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # The eight routes could be made by next line
+  # resources :users
+  get 'users/:id', to: 'users#show', as: 'user' 
+  get 'users', to: 'users#index', as: 'users' 
+  post 'users', to: 'users#create' 
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user' 
+end
