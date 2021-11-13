8fddd5bbffbd003e142280054911c594becb6a24
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-13 16:19:02 +0200

    Change king symbol

--- Ruby/chess_game/board.rb
+++ Ruby/chess_game/board.rb
@@ -1,14 +1,15 @@
 require_relative 'pieces'
 require 'byebug'
 class Board
-    
-    def init_board(full_board)
+    attr_reader :rows 
+
+    def init_board(fill_board)
         board = Array.new(8){Array.new(8, nullPiece)}
-        if full_board
-            board[0] = make_back_row(:white)
-            board[7] = make_back_row(:black)
-            board[1] = make_pawn_row(:white)
-            board[6] = make_pawn_row(:black)
+        if fill_board
+            board[0] = make_back_row(:blue)     # I used blue/yellow colors instead of white/black to be visible on CLI
+            board[7] = make_back_row(:yellow)
+            board[1] = make_pawn_row(:blue)
+            board[6] = make_pawn_row(:yellow)
         end
         board
     end
@@ -16,7 +17,7 @@ class Board
     def make_back_row(color)
         pieces_classes = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
         back_row = []
-        i = (color == :white ? 0 : 7)
+        i = (color == :blue ? 0 : 7)
         pieces_classes.each_with_index do |piece_class, j|
             back_row << piece_class.new(self, [i, j], color)
         end
@@ -24,15 +25,15 @@ class Board
     end
 
     def make_pawn_row(color)
-        r = (color == :white ? 1 : 6)
+        r = (color == :blue ? 1 : 6)
         row = []
         8.times{ |c| row << Pawn.new(self,[r, c], color)}
         row 
     end
 
-    def initialize(full_board = true)
+    def initialize(fill_board = true)
         @nullPiece = NullPiece.instance
-        @rows = init_board(full_board)
+        @rows = init_board(fill_board)
     end
 
     def [](pos)
@@ -50,14 +51,14 @@ class Board
     
     def move_piece (turn_color, start_pos, end_pos)
         piece = self[start_pos]
-        raise "You can only move your own pieces" if piece.color != turn_color
-        raise "start position #{start_pos} is empty.." if empty?(start_pos)
-        raise "This move is not allowed #{end_pos}.." unless piece.moves.include?(end_pos)
-        raise "You can not move into check" unless piece.valid_moves.include?(end_pos)
+        raise RuntimeError.new("start position #{start_pos} is empty..") if empty?(start_pos)
+        raise RuntimeError.new("You can only move your own pieces") if piece.color != turn_color
+        raise RuntimeError.new("This move #{end_pos} is not allowed for #{piece.symbol}.") unless piece.moves.include?(end_pos)
+        raise RuntimeError.new("You can not move into check") unless piece.valid_moves.include?(end_pos)
         move_piece!(start_pos, end_pos)
     end
 
-    #moves piece without checking in_check? .. used in piece class
+    #moves piece without checking in_check? .. called in piece class
     def move_piece!(start_pos, end_pos)
         piece = self[start_pos]
         self[end_pos] = piece 
@@ -79,73 +80,40 @@ class Board
         player_pieces.all?{|piece| piece.valid_moves.empty?}
     end
 
-    def pieces (color)
+    def pieces (color)        
         rows.flatten.select{|piece| piece.color == color}
     end
 
     def in_check?(color)
         king_pos = find_king(color)
-        if color == :white
-            opponent_pieces = pieces(:black)
+        if color == :blue
+            opponent_pieces = pieces(:yellow)
         else
-            opponent_pieces = pieces(:white)
+            opponent_pieces = pieces(:blue)
         end
-        # opponent_pieces = pieces(color == :white? :black : :white)
         opponent_pieces.any?{|piece| piece.moves.include?(king_pos)}
     end
 
     def find_king (color)
         rows.flatten.each do |piece|
-            return piece.pos if piece.color == color && piece.symbol == :King 
+            return piece.pos if piece.color == color && piece.symbol == :K 
         end
         nil 
     end
 
-    def dup_row!(row,i, new_board)
-        rows[i].each_with_index do |piece, p_i|
-            row[p_i] = piece.class.new(new_board, piece.pos, piece.color) unless piece.empty?
+    def dup_row!(row,i, new_board)     # fill a row in duplicate board
+        rows[i].each_with_index do |piece, piece_idx|
+            row[piece_idx] = piece.class.new(new_board, piece.pos, piece.color) unless piece.empty?
         end
     end
 
-    def dup 
+    def dup      # returns a duplicate board of current board
         new_board = Board.new(false)    # creates board of nullPieces
         new_board.rows.each_with_index {|new_row, i| dup_row!( new_row, i, new_board)}
         new_board
     end
 
-    
-    attr_reader :rows
     private
     attr_reader :nullPiece
 end
-
-# b = Board.new 
-
-# b.move_piece(:white, [1, 4], [3, 4])
-
-# b.rows.each do |row|
-#     p row.map(&:symbol)
-# end
-# puts
-
-# b.move_piece(:black, [6, 5], [5, 5])
-
-# b.rows.each do |row|
-#     p row.map(&:symbol)
-# end
-# puts
-# b.move_piece(:black, [6 , 6], [4, 6])
-
-# b.rows.each do |row|
-#     p row.map(&:symbol)
-# end
-# puts
-# b.move_piece(:white, [0, 3], [4, 7])
-
-# b.rows.each do |row|
-#     p row.map(&:symbol)
-# end
-# puts
-
-# p b.check_mate?(:black)
 
